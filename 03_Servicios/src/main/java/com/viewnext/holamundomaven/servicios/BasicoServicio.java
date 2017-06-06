1a3c1b44138e7c61a1fa5d02b8d93621aab62fc3
Victor <victorherrerocazurro@gmail.com>
2017-06-06 20:33:23 +0200

    Dia 2

---
+++ 03_Servicios/src/main/java/com/viewnext/holamundomaven/servicios/BasicoServicio.java
@@ -0,0 +1,12 @@
+package com.viewnext.holamundomaven.servicios;
+
+//@Service
+public class BasicoServicio implements Servicio{
+
+	@Override
+	public void altaUsuario() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
